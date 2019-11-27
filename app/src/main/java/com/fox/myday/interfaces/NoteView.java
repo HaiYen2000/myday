6f2af159e9962b7d4820f7b5195e03c9c16d08c2
lamlt <yenchi2232000@gmail.com>
2019-11-28 00:11:02 +0700

    Update MVP for note function

--- app/src/main/java/com/fox/myday/interfaces/NoteView.java
+++ app/src/main/java/com/fox/myday/interfaces/NoteView.java
@@ -1,4 +1,17 @@
 package com.fox.myday.interfaces;
 
 public interface NoteView {
+
+    void onCreateNoteSuccess();
+
+    void onCreateNoteFail();
+
+    void onEditNoteSuccess();
+
+    void onEditNoteFail();
+
+    void onDeleteSuccess();
+
+    void onDeleteFail();
+
 }
--- app/src/main/java/com/fox/myday/presenters/NotePresenter.java
+++ app/src/main/java/com/fox/myday/presenters/NotePresenter.java
@@ -1,4 +1,29 @@
 package com.fox.myday.presenters;
 
+import com.fox.myday.interfaces.NoteView;
+
 public class NotePresenter {
+
+    NoteView noteView;
+
+    public NotePresenter(NoteView noteView) {
+        this.noteView = noteView;
+    }
+
+    public void onCreateNote(){
+
+    }
+
+    public void onEditNote(){
+
+    }
+
+    public void onDeleteNote(){
+
+    }
+
+    public void onViewDetailNote(){
+
+    }
+
 }
